b562e10100443714921dcd28a9f82f8061ea53aa
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2023-01-08 23:24:25 +0000

    can change selected square

--- Megaprocessor asm/handle_input.asm
+++ Megaprocessor asm/handle_input.asm
@@ -3,6 +3,7 @@ IO_SWITCH_FLAG_UP               EQU     0x0001;
 IO_SWITCH_FLAG_DOWN             EQU     0x0002;
 IO_SWITCH_FLAG_LEFT             EQU     0x0004;
 IO_SWITCH_FLAG_RIGHT            EQU     0x0008;
+IO_SWITCH_FLAG_CROSS            EQU     0x0080;
 
                                          // ** R0 = input
                                          // void handle_input(input) {
@@ -16,7 +17,7 @@ AND R1,R0;
 BNE handle_input_up_is_not_pressed;      //     if(GEN_IO_INPUT & IO_SWITCH_FLAG_UP != 0) {
                                          //     } else {
 LD.B R3,global_cursor_square_index;      //         var newCursorIndex = global_cursor_square_index;
-MOVE R1, R3;                             //         var oldCursorIndex = global_cursor_square_index;
+MOVE R1, R3;                             //         var oldCursorIndex = newCursorIndex;
 LD.B R2,#10;
 SUB R3,R2;                               //         newCursorIndex -= 10;
 JMP move_if_new_cursor_still_on_board;   //         move_if_new_cursor_still_on_board(oldCursorIndex);
@@ -28,7 +29,7 @@ AND R1,R0;
 BNE handle_input_down_is_not_pressed;    //     if(GEN_IO_INPUT & IO_SWITCH_FLAG_DOWN != 0) {
                                          //     } else {
 LD.B R3,global_cursor_square_index;      //         var newCursorIndex = global_cursor_square_index;
-MOVE R1, R3;                             //         var oldCursorIndex = global_cursor_square_index;
+MOVE R1, R3;                             //         var oldCursorIndex = newCursorIndex;
 LD.B R2,#10;
 ADD R3,R2;                               //         newCursorIndex += 10;
 JMP move_if_new_cursor_still_on_board;   //         move_if_new_cursor_still_on_board(oldCursorIndex);
@@ -40,7 +41,7 @@ AND R1,R0;
 BNE handle_input_left_is_not_pressed;    //     if(GEN_IO_INPUT & IO_SWITCH_FLAG_LEFT != 0) {
                                          //     } else {
 LD.B R3,global_cursor_square_index;      //         var newCursorIndex = global_cursor_square_index;
-MOVE R1, R3;                             //         var oldCursorIndex = global_cursor_square_index;
+MOVE R1, R3;                             //         var oldCursorIndex = newCursorIndex;
 LD.B R2,#1;
 SUB R3,R2;                               //         newCursorIndex -= 1;
 JMP move_if_new_cursor_still_on_board;   //         move_if_new_cursor_still_on_board(oldCursorIndex);
@@ -52,12 +53,25 @@ AND R1,R0;
 BNE handle_input_right_is_not_pressed;   //     if(GEN_IO_INPUT & IO_SWITCH_FLAG_RIGHT != 0) {
                                          //     } else {
 LD.B R3,global_cursor_square_index;      //         var newCursorIndex = global_cursor_square_index;
-MOVE R1, R3;                             //         var oldCursorIndex = global_cursor_square_index;
+MOVE R1, R3;                             //         var oldCursorIndex = newCursorIndex;
 LD.B R2,#1;
 ADD R3,R2;                               //         newCursorIndex -= 1;
 JMP move_if_new_cursor_still_on_board;   //         move_if_new_cursor_still_on_board(oldCursorIndex);
                                          //         return;
 handle_input_right_is_not_pressed:       //     }
+
+LD.W R1,#IO_SWITCH_FLAG_CROSS;
+AND R1,R0;
+BNE handle_input_cross_is_not_pressed;   //     if(GEN_IO_INPUT & IO_SWITCH_FLAG_CROSS != 0) {
+                                         //     } else {
+LD.B R1,global_selected_square_index;    //         var oldSelectedIndex = global_selected_square_index;
+LD.B R3,global_cursor_square_index;
+ST.B global_selected_square_index,R3;    //         global_selected_square_index = global_cursor_square_index;
+LD.W R0, #handle_input_return;
+JMP draw_piece;                          //         draw_piece(old_square_index); // clear selected from old square
+                                         //         return;
+
+handle_input_cross_is_not_pressed:       //     }
 
 JMP handle_input_return;                 //     return;
                                          // }
